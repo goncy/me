d9eb292fd4f9c311c5375e1765f347274a3ec5ca
goncy <gonzalo.pozzo4@gmail.com>
2022-02-28 13:22:42 -0300

    Update tags

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -23,14 +23,14 @@ function App({Component, pageProps}: AppProps) {
   return (
     <>
       <Head>
-        <title>Communcy</title>
+        <title>Gonzalo Pozzo</title>
         <meta content="initial-scale=1.0, width=device-width" name="viewport" />
         {/* Start meta tags */}
-        <meta content="Communcy" name="title" />
-        <meta content="goncy" name="author" />
+        <meta content="Gonzalo Pozzo" name="title" />
         <meta content="Gonzalo Pozzo" name="copyright" />
+        <meta content="goncy" name="author" />
         <meta
-          content="Communcy es un portal de descubrimiento sobre gente de la comunidad, con información y tips para gente que está en la búsqueda laboral o perfiles para empresas que buscan nuevos candidatos"
+          content="I'm Gonzalo Pozzo, a frontend developer and content creator from Argentina, focused in helping people to land their first job in IT"
           name="description"
         />
         <meta content="gray" name="theme-color" />
